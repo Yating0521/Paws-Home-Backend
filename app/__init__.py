35d8738d38552d82fb2d08810e8c7c8e6c138e51
lyt <2964936320@qq.com>
2025-08-14 12:56:01 +0800

    change url

--- app/__init__.py
+++ app/__init__.py
@@ -8,11 +8,17 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     
-    database_url = os.getenv("DATABASE_URL")
-    if not database_url:
-        raise ValueError("DATABASE_URL is not set!")
+    # database_url = os.getenv("DATABASE_URL")
+    # if not database_url:
+    #     raise ValueError("DATABASE_URL is not set!")
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+    # app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+
+    if app.config['SQLALCHEMY_DATABASE_URI'].startswith('postgresql://'):
+        app.config['SQLALCHEMY_DATABASE_URI'] = app.config['SQLALCHEMY_DATABASE_URI'].replace('postgresql://', 'postgresql+psycopg2://', 1)
+    
     db.init_app(app)
     CORS(app)
 
